df29630fc2080a4008396cc2193868815a77a438
eobodyanik <eobodyanik@getnerdio.com>
2020-02-05 21:42:01 -0600

    added command line argument processing

--- src/main/java/edu/htc/blitstein/HelloWorld.java
+++ src/main/java/edu/htc/blitstein/HelloWorld.java
@@ -1,7 +1,11 @@
 package edu.htc.blitstein;
 
 public class HelloWorld {
-    public static void main(String... argv){
+    public static void main(String... args){
+        String name = "Bob";
+        if (args.length > 0) {
+            name = args[0];
+        }
         System.out.println("Hello, world");
     }
 
